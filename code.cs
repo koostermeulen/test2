ef150814e6fe616f325c8ebc95b00a21839a72a1
koos <koos>
2014-10-31 17:07:27 +0100

    aa

--- code.cs
+++ code.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Linq;
+
 public class Hello
 {
+  //coment
   static void Main()
   {
+    Console.WriteLine("Hi World");
     Console.WriteLine("Hello World");
+    Console.WriteLine("Hi World")
   }
 }
