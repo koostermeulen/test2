43a078f66e90b2eaf7f3880878c82da38cbd4fc7
koos <koos>
2014-10-31 17:09:14 +0100

    bad change

--- code.cs
+++ code.cs
@@ -6,6 +6,8 @@ public class Hello
   //coment
   static void Main()
   {
+	//more commetns
+	while(tru)
     Console.WriteLine("Hi World");
     Console.WriteLine("Hello World");
     Console.WriteLine("Hi World")
